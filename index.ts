c4e4cfa54bdc05b8ae2ee0d9f7bb80dd8b247e61
Krzysztof Iwanski <k84iwanski@gmail.com>
2023-08-29 20:07:49 +0200

    Added Jest and first tests to project.

--- index.ts
+++ index.ts
@@ -19,5 +19,5 @@ app.use(json());
 app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
-    console.log('Listening on http://localhost:/3001');
+    console.log('Listening on http://localhost:3001');
 });
